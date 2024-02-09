03afc0c73f6bba518c788b00d3c8aefd9a9c439c
karthik2 <karthik2@gmail.com>
2024-02-08 17:07:59 -0800

    docker build groovy added

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,7 @@
+def call(String project, String hubUser, String ImageTag){
+    sh """
+        docker build -t ${hubUser}/${project} .
+        docker image tag ${hubUser}/${project} ${hubUser}:${project}:${ImageTag}
+        docker image tag ${hubUser}/${project} ${hubUser}:${project}:latest
+    """
+}
